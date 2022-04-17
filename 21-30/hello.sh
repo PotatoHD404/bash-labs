e9aecab23ee34736673b629bbf6fc37566e69c10
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-04-17 19:07:24 +0300

    tested some of the who numbers, 34 is not working and added +x to all

--- 21-30/hello.sh
+++ 21-30/hello.sh
@@ -2,17 +2,18 @@
 
 if [ "$#" -ne 1 ]
 then
-        echo 'in next time write "-d" to see date'
-        echo 'in next time write "-w" to see workind users'
-        echo 'in next time write "-l" to see files in this directory'
+        echo 'next time write:'
+        echo '"-d" to see date'
+        echo '"-w" to see working users'
+        echo '"-l" to see files in this directory'
 else
         case $1 in
         -d) date;;
         -w) who;;
         -l) ls;;
-        *) echo 'in next time write:'
+        *) echo 'next time write:'
            echo '"-d" to see date'
-           echo '"-w" to see workind users'
+           echo '"-w" to see working users'
            echo '"-l" to see files in this directory' ;;
         esac
 fi
--- 31-40/34.sh
+++ 31-40/34.sh
@@ -2,12 +2,12 @@
 
 who | tr -s ' ' | cut -f1 -d' ' > tmp 
 x=$(wc -l < tmp) 
-let z=$x 
+let z = $x 
 echo 'input' 
 read message 
 while [ "$z" -gt 0 ] 
 do 
-    y=$(tail -1 tmp) 
+    y=$(tail -n1 tmp) 
     y1=${y}' ' 
     y1=${y1%%' '} 
     let z=$z-1 
